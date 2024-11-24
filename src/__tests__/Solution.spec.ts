59bf058a518a339b28d046892f5446938ba61abf
MeriLevente <meri.levente@students.jedlik.eu>
2024-11-24 21:39:35 +0100

    1.feladat tesztelése

--- src/__tests__/Solution.spec.ts
+++ src/__tests__/Solution.spec.ts
@@ -1,1 +1,9 @@
 import Solution from "../Solution"
+
+const sol: Solution = new Solution("valaszok.txt")
+
+describe("1.feladat tesztelése", ()=>{
+    it("Az adatokat beolvasása sikere, a contenders vektor nem üres", ()=>{
+        expect(sol.getContenders.length).toBeGreaterThan(0)
+    })
+})
